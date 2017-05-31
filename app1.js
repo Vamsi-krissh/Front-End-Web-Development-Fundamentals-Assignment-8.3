89770c7f0e571d2cbe371622fe82f88035855e78
Vamsi-krissh <vamsi1393@gmail.com>
2017-05-31 16:54:41 +0530

    Assignment 8.3

---
+++ app1.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){           
+    $("input").click(function(){        //Fire the event when input has been clicked
+        $("input").hide();               // hides all the input fields
+   
+    });
+});
